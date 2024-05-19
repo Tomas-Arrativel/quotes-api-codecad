6617843cb291f18142aee93c9371fdf20994861c
Tomás Arrativel <arrativeltomas@gmail.com>
2024-05-19 17:17:20 -0300

    Making the get request for all the quotes or from an author

--- server.js
+++ server.js
@@ -10,7 +10,21 @@ app.use(express.static('public'));
 
 app.get('/api/quotes/random', (req, res, next) => {
   const quoteToSend = getRandomElement(quotes);
-  res.json({ quote: quoteToSend });
+  res.send({ quote: quoteToSend });
+});
+
+app.get('/api/quotes', (req, res, next) => {
+  if (req.query.person) {
+    const person = req.query.person;
+    const quotesToSend = quotes.filter((quote) =>
+      quote.person.includes(person),
+    );
+
+    if (quotesToSend.length > 0) res.send({ quotes: quotesToSend });
+    else res.send({ quotes: [] });
+  } else {
+    res.send({ quotes: quotes });
+  }
 });
 
 app.listen(PORT, () => {
